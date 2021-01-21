8fcf4e95cdc45b5d2762b900b869c0ed4795cb82
lemonQwQ <1282920248@qq.com>
2021-01-21 14:54:16 +0800

    first commit

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "<h1>Hello, 这里是 ggoblog</h1>")
+    fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
 }
 func main() {
     http.HandleFunc("/", handlerFunc)
