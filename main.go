5fd960e27a2a2b8abce0f8e8764173c5f7804f03
lemonQwQ <1282920248@qq.com>
2021-01-22 13:23:19 +0800

    设置标头

--- main.go
+++ main.go
@@ -10,10 +10,11 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	// w.WriteHeader(http.StatusInternalServerError)
 	// w.Header().Set("name", "my name is lemon")
 	// fmt.Fprint(w, "请求路径为:"+r.URL.Path)
+	w.Header().Set("Content-Type", "text/html; charset=utf-8") //标头设置
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog</h1>")
 	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "博客用以记录笔记，如有反馈请联系："+"<a href=\"#\">tt</a>")
+		fmt.Fprint(w, "博客用以记录笔记，如有反馈请联系："+"<a href=\"https://www.baidu.com\">百度</a>")
 	} else {
 		fmt.Fprintf(w, "<h1>请求页面为找到:(</h1>")
 	}
