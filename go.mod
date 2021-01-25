d2cf826eb5ff4e82cf1cc44f2a813830691a7216
lemonQwQ <1282920248@qq.com>
2021-01-25 22:21:22 +0800

    连接数据库

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/lemonQwQ/goblog
 go 1.15
 
 require (
-	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gorilla/mux v1.8.0
 	golang.org/x/tools v0.1.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 	"net/url"
 	"strings"
 	"text/template"
+	"time"
 	"unicode/utf8"
 
+	"github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
@@ -18,6 +23,42 @@ type ArticlesFormData struct {
 }
 
 var router = mux.NewRouter()
+var db *sql.DB
+
+func initDB() {
+	var err error
+	config := mysql.Config{
+		User:                 "root",
+		Passwd:               "12345678",
+		Addr:                 "127.0.0.1:3306",
+		Net:                  "tcp",
+		DBName:               "goblog",
+		AllowNativePasswords: true,
+	}
+
+	// 准备数据库连接池
+	// DSN => Data Source Name
+	db, err = sql.Open("mysql", config.FormatDSN())
+	checkError(err)
+
+	// 设置最大连接数
+	db.SetMaxOpenConns(25)
+	// 设置最大空闲连接数
+	db.SetMaxIdleConns(25)
+
+	// 设置每个链接的过期时间
+	db.SetConnMaxLifetime(5 * time.Minute)
+
+	// 尝试连接，失败报错
+	err = db.Ping()
+	checkError(err)
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog</h1>")
@@ -162,6 +203,8 @@ func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	initDB()
+
 	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
 	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
 
@@ -176,10 +219,10 @@ func main() {
 	router.Use(forceHTMLMiddleware)
 
 	// 通过命名路由获取URL示例
-	homeURL, _ := router.Get("home").URL()
+	/*homeURL, _ := router.Get("home").URL()
 	fmt.Println("homeURL: ", homeURL)
 	articlesURL, _ := router.Get("articles.show").URL("id", "233")
-	fmt.Println("articlesURL: ", articlesURL)
+	fmt.Println("articlesURL: ", articlesURL)*/
 
 	http.ListenAndServe(":3000", removeTrailingSlash(router))
 }
