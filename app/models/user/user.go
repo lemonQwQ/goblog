7cecade2481451dbe412a723b7383bcdf1b7621f
lemonQwQ <1282920248@qq.com>
2021-02-02 23:09:30 +0800

    用户密码加密

--- app/models/user/user.go
+++ app/models/user/user.go
@@ -1,6 +1,9 @@
 package user
 
-import "goblog/app/models"
+import (
+	"goblog/app/models"
+	"goblog/pkg/password"
+)
 
 // User 用户模型
 type User struct {
@@ -14,6 +17,6 @@ type User struct {
 }
 
 // ComparePassword 对比密码是否匹配
-func (u User) ComparePassword(password string) bool {
-	return u.Password == password
+func (u User) ComparePassword(_password string) bool {
+	return password.CheckHash(_password, u.Password)
 }
