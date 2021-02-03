511eb0392d4188e2cd09152639aaad7c566672f3
lemonQwQ <1282920248@qq.com>
2021-02-03 17:18:03 +0800

    文章作者

--- app/models/article/article.go
+++ app/models/article/article.go
@@ -2,6 +2,7 @@ package article
 
 import (
 	"goblog/app/models"
+	"goblog/app/models/user"
 	"goblog/pkg/route"
 	"strconv"
 )
@@ -10,11 +11,18 @@ import (
 type Article struct {
 	models.BaseModel
 
-	Title string `valid:"title"`
-	Body  string `valid:"body"`
+	Title  string `valid:"title"`
+	Body   string `valid:"body"`
+	UserID uint64 `gorm:"not null;index"`
+	User   user.User
 }
 
 // Link 方法用来生成文章链接
 func (a Article) Link() string {
 	return route.Name2URL("articles.show", "id", strconv.FormatUint(a.ID, 10))
+}
+
+// CreatedAtDate 创建日期
+func (a Article) CreatedAtDate() string {
+	return a.CreatedAt.Format("2006-01-02")
 }
